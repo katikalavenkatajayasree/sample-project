67639c80747ca3bc602e900c70ef7e5a7c6775bb
katikalavenkatajayasree <katikalavenkatajayasree@gmail.com>
2026-03-12 12:09:18 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let a=10
+let b=12
+let choice="*"
+switch(choice){
+    case "+":
+        {console.log("addition is:",a+b)
+        break
+        }
+    case "-":{
+            console.log("sub is:",a-b)
+            break
+             }
+     case "*" :{
+        console.log("mul is:",a*b)
+        break
+     }     
+    default:
+        console.log("invalid choice")
+}
